0d7ef9d8ee5baaf9c9a5a4e2a54f9b8d50afe63d
m21br <lbr3021@gmail.com>
2023-01-29 11:06:31 +0900

    final styles

--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,9 +1,12 @@
-import 'styled-components';
+import "styled-components";
 
-declare module 'styled-components' {
+declare module "styled-components" {
   export interface DefaultTheme {
     bgColor: string;
     cardColor: string;
     boardColor: string;
+    isDraggingOver: string;
+    isDraggingFromThis: string;
+    isDragging: string;
   }
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,7 +1,10 @@
-import { DefaultTheme } from 'styled-components';
+import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
-  bgColor: '#3F8CF2',
-  boardColor: '#DADFE9',
-  cardColor: 'white',
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
+  isDraggingOver: "#dfe6e9",
+  isDraggingFromThis: "#b2bec3",
+  isDragging: "#e4f2ff",
 };
